51dfb76ef303afa6f4db823d511cd0741648dc9c
Noah Shan <luth595@gmail.com>
2024-08-19 16:24:55 +0530

    added CreateOrder functionality
    Changes to be committed:
    	modified:   src/App.jsx
    	modified:   src/features/order/CreateOrder.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu, {loader as menuLoader} from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/createOrder";
-import Order,{ loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/createOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -30,6 +32,8 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
       },
       {
         path: "/order/:orderId",
