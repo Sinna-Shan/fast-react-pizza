e8eb680f8f91fbb099cf83a27ebefe4d3f2c2de0
Noah Shan <luth595@gmail.com>
2024-08-19 22:39:45 +0530

    css box model
    Changes to be committed:
    	modified:   src/features/cart/CartOverview.jsx
    	modified:   src/features/user/CreateUser.jsx
    	modified:   src/features/user/UserName.jsx
    	modified:   src/ui/Header.jsx
    	modified:   src/ui/Home.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,10 @@ import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
-      <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
       <UserName />
     </header>
