90a69233c498461a7890393d08e2d081f08816c9
Noah Shan <luth595@gmail.com>
2024-08-21 12:53:31 +0530

    added cartSlice
    Changes to be committed:
    	new file:   src/features/cart/cartSlice.js
    	modified:   src/store.js

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //   payload = new Item
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  decreaseItemQuantity,
+  increaseItemQuantity,
+  clearCart,
+  deleteItem,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
